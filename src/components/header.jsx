35cc2d129155ff6622bb31b964743df6d9bc566d
mohamad sajadi <mohamadcodew84@gmail.com>
2025-03-26 16:28:26 +0330

    header of project

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="mt-[50px] text-center">
+        <h1 className="text-2xl font-bold text-[#304ffe] mb-[20px]">Contact App</h1>
+        <p className="text-gray-700 ">
+            <a  className=' transition-all duration-500 text-[#304ffe] bg-[#bbdefb] py-[3px] rounded-[5px] px-[7px] hover:bg-[#304ffe] hover:text-[#bbdefb] ' href="https://github.com/mhdcode">Github</a> |  React.js Full Course
+            </p>
+    </div>
+  )
+}
+
+export default Header;
