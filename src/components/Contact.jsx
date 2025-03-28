2412a00de33f99fdbf7b706d7fe9980f9f1e77b9
mohamad sajadi <mohamadcodew84@gmail.com>
2025-03-29 01:42:11 +0330

    add contact to contacts array

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 function Contact() {
+  const [contacts,setcontacts] = useState([])
 const [contact,setcontact] = useState({
   name:"",
   lastname:"",
@@ -15,8 +16,17 @@ const value = event.target.value
 setcontact((contact)=>({...contact ,[name]:value }))
 
 }
-function loginhandler(){
-console.log(contact);
+function addhandler(){
+setcontacts((contacts)=>({...contacts,contact}))
+setcontact(
+{  name:"",
+  lastname:"",
+  email:"",
+  number:"",
+}
+)
+console.log(contacts);
+
 
 }
   return (
@@ -25,8 +35,8 @@ console.log(contact);
             <input className='border' type="text" placeholder='Name'  onChange={changehandler} name='name' value={contact.name} />
             <input className='border' type="text" placeholder='Last Name'onChange={changehandler} name='lastname'  value={contact.lastname} />
             <input className='border' type="email" placeholder='Email'  onChange={changehandler} name='email' value={contact.email}/>
-            <input className='border' type="number" placeholder='Phone'  onChange={changehandler} name='number' value={contact.pho}/>
-            <button onClick={loginhandler} className='bg-amber-200 rounded-sm' type='button'>click</button>
+            <input className='border' type="number" placeholder='Phone'  onChange={changehandler} name='number' value={contact.number}/>
+            <button onClick={addhandler} className='bg-amber-200 rounded-sm' type='button'>Add contact</button>
         </div>
     </div>
   )
